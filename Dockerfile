1f66352c61d2cae57ff47554e80ac5fb30c00c98
Zak Espley <zespley@physics.ucsb.edu>
2020-04-01 19:08:05 -0700

    Removed print from video filter,

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN apt-get install -y git
 RUN useradd -rm -d /home/phys phys
 RUN cd /opt && git clone https://github.com/ilg-physics-ucsb/ManualLayout.git
 RUN chmod +x /opt/ManualLayout/compile
+RUN cp -R /opt/ManualLayout/css /home/phys
 USER phys
 WORKDIR /home/phys
 RUN echo "export PATH=/opt/ManualLayout:$PATH" > ~/.bashrc
--- filters/gvideo_block.py
+++ filters/gvideo_block.py
@@ -6,7 +6,6 @@ def prepare(doc):
 
 def action(elem, doc):
     if isinstance(elem, pf.Div) and "GVideo" in elem.classes:
-        print("Inside")
         gvideo = elem
         file_id = gvideo.content[0].content[0].text
         video = """
@@ -15,8 +14,6 @@ def action(elem, doc):
         <figcaption>Video {1}
         </figure>
         """.format(file_id, doc.latest_video)
-        # parent.content[gvideo.index] = 
-        # gvideo = pf.convert_text(gvideo, input_format="html")
         gvideo = pf.RawBlock(video)
         doc.latest_video += 1
         return gvideo
